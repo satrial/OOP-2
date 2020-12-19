28b8cb7f9ab46e3204ac294d5de3c15f1027c4ed
wisnu518 <wisnuwisnu466@gmail.com>
2020-12-19 20:24:31 +0700

    anggota 2

---
+++ app/src/main/java/com/satria/oopcoba/UserAdapter.kt
@@ -0,0 +1,46 @@
+package com.satria.oopcoba
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.satria.oopcoba.Database.User
+import com.satria.opp.Database.Laptop
+import kotlinx.android.synthetic.main.adapter_user.view.*
+
+class UserAdapter (private val AllUser: ArrayList<User>, private val listener: OnAdapterListener) : RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        return UserViewHolder(
+            LayoutInflater.from(parent.context).inflate( R.layout.adapter_user, parent, false)
+        )
+    }
+
+    override fun getItemCount() = AllUser.size
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        val user = AllUser[position]
+        holder.view.text_username.text = user.username
+        holder.view.text_username.setOnClickListener {
+            listener.onClick(user)
+        }
+
+        holder.view.icon_delete.setOnClickListener {
+            listener.onDelete(user)
+        }
+    }
+
+    class UserViewHolder(val view: View) : RecyclerView.ViewHolder(view)
+
+    fun setData(list: List<User>) {
+        AllUser.clear()
+        AllUser.addAll(list)
+        notifyDataSetChanged()
+    }
+
+    interface OnAdapterListener {
+        fun onClick(user: User)
+        fun onDelete(user: User)
+    }
+
+}
